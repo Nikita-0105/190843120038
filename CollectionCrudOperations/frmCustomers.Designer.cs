d6ed9e674d3ace2f15829b03541187a4fdb24dda
group022 <group022@infoway.com>
2019-12-30 12:23:02 +0530

    .net project

---
+++ CollectionCrudOperations/frmCustomers.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace CollectionCrudOperations
+{
+    partial class frmCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.btnMoveFirst = new System.Windows.Forms.Button();
+            this.btnMovePrevious = new System.Windows.Forms.Button();
+            this.btnMoveNext = new System.Windows.Forms.Button();
+            this.btnMoveLast = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Infoway Customer Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(175, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(175, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(175, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "City";
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustomerId.Location = new System.Drawing.Point(335, 115);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(186, 26);
+            this.txtCustomerId.TabIndex = 4;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContactName.Location = new System.Drawing.Point(335, 159);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(269, 26);
+            this.txtContactName.TabIndex = 5;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(335, 201);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(269, 26);
+            this.txtCity.TabIndex = 6;
+            // 
+            // btnMoveFirst
+            // 
+            this.btnMoveFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveFirst.Location = new System.Drawing.Point(158, 279);
+            this.btnMoveFirst.Name = "btnMoveFirst";
+            this.btnMoveFirst.Size = new System.Drawing.Size(96, 52);
+            this.btnMoveFirst.TabIndex = 7;
+            this.btnMoveFirst.Text = "<<";
+            this.btnMoveFirst.UseVisualStyleBackColor = true;
+            this.btnMoveFirst.Click += new System.EventHandler(this.btnMoveFirst_Click);
+            // 
+            // btnMovePrevious
+            // 
+            this.btnMovePrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMovePrevious.Location = new System.Drawing.Point(274, 279);
+            this.btnMovePrevious.Name = "btnMovePrevious";
+            this.btnMovePrevious.Size = new System.Drawing.Size(96, 52);
+            this.btnMovePrevious.TabIndex = 8;
+            this.btnMovePrevious.Text = "<";
+            this.btnMovePrevious.UseVisualStyleBackColor = true;
+            this.btnMovePrevious.Click += new System.EventHandler(this.btnMovePrevious_Click);
+            // 
+            // btnMoveNext
+            // 
+            this.btnMoveNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveNext.Location = new System.Drawing.Point(391, 279);
+            this.btnMoveNext.Name = "btnMoveNext";
+            this.btnMoveNext.Size = new System.Drawing.Size(96, 52);
+            this.btnMoveNext.TabIndex = 9;
+            this.btnMoveNext.Text = ">";
+            this.btnMoveNext.UseVisualStyleBackColor = true;
+            this.btnMoveNext.Click += new System.EventHandler(this.btnMoveNext_Click);
+            // 
+            // btnMoveLast
+            // 
+            this.btnMoveLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveLast.Location = new System.Drawing.Point(515, 279);
+            this.btnMoveLast.Name = "btnMoveLast";
+            this.btnMoveLast.Size = new System.Drawing.Size(96, 52);
+            this.btnMoveLast.TabIndex = 10;
+            this.btnMoveLast.Text = ">>";
+            this.btnMoveLast.UseVisualStyleBackColor = true;
+            this.btnMoveLast.Click += new System.EventHandler(this.btnMoveLast_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(515, 346);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(96, 52);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Location = new System.Drawing.Point(391, 346);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(96, 52);
+            this.btnModify.TabIndex = 13;
+            this.btnModify.Text = "&Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(274, 346);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(96, 52);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(158, 346);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(96, 52);
+            this.btnNew.TabIndex = 11;
+            this.btnNew.Text = "&New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // frmCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnMoveLast);
+            this.Controls.Add(this.btnMoveNext);
+            this.Controls.Add(this.btnMovePrevious);
+            this.Controls.Add(this.btnMoveFirst);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCustomers";
+            this.Text = "Infoway Customer Form";
+            this.Load += new System.EventHandler(this.frmCustomers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button btnMoveFirst;
+        private System.Windows.Forms.Button btnMovePrevious;
+        private System.Windows.Forms.Button btnMoveNext;
+        private System.Windows.Forms.Button btnMoveLast;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
---
+++ CollectionCrudOperations/frmCustomers.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CollectionCrudOperations.Models;
+
+namespace CollectionCrudOperations
+{
+    public partial class frmCustomers : Form
+    {
+        List<Customer> customersCollection = new List<Customer>();
+        int currentIndex = 0;
+        public frmCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomers_Load(object sender, EventArgs e)
+        {
+            Navigation(currentIndex);
+        }
+
+        private void btnMoveFirst_Click(object sender, EventArgs e)
+        {
+            currentIndex = 0;
+            Navigation(currentIndex);
+        }
+        private void Navigation(int index)
+        {
+            if (customersCollection.Count>0)
+            {
+                txtCustomerId.Text = customersCollection[index].CustomerId.ToString();
+                txtContactName.Text = customersCollection[index].ContactName;
+                txtCity.Text = customersCollection[index].City;
+            }
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtCustomerId.Text = string.Empty;
+            txtContactName.Text = string.Empty;
+            txtCity.Text = string.Empty;
+            txtCustomerId.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Customer newCustomer = new Customer()
+            {
+                CustomerId=int.Parse(txtCustomerId.Text),
+                ContactName=txtContactName.Text,
+                City=txtCity.Text
+            };
+            customersCollection.Add(newCustomer);
+        }
+
+        private void btnMoveLast_Click(object sender, EventArgs e)
+        {
+            currentIndex = customersCollection.Count-1;
+            Navigation(currentIndex);
+        }
+
+        private void btnMovePrevious_Click(object sender, EventArgs e)
+        {
+            if (currentIndex>0)
+            {
+                currentIndex--;
+                Navigation(currentIndex);
+            }
+        }
+
+        private void btnMoveNext_Click(object sender, EventArgs e)
+        {
+            if (currentIndex<customersCollection.Count-1)
+            {
+                currentIndex++;
+                Navigation(currentIndex);
+            }
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            customersCollection[currentIndex].ContactName = txtContactName.Text;
+            customersCollection[currentIndex].City = txtCity.Text;
+        }
+    }
+}
